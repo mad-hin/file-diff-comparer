8dc35ea0d563b3a2b3890e134ab7b018551139fc
Marco Tam <marcotamcn@gmail.com>
2023-04-08 17:30:29 +0800

    Finished textarea diff compare

--- README.md
+++ README.md
@@ -1,38 +1,34 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# File Diff Comparer
 
-## Getting Started
+## Abstract
+A file comparer which compares two files and shows the differences between them. It is a simple application build with Next.js and TailwindCSS.
 
-First, run the development server:
+## Aim of the project
+The first aim of the project is due to one of the programming courses I am taking in HKUST no longer provide instantaneous feedback on the correctness of the code in this semester (2023 Spring) and I have to compare the output of my code with the expected output manually. This is a tedious and time-consuming task. Therefore, I decided to build a simple application to help me with this task and as a practice project. Another aim of the project is to learn how to use Next.js and TailwindCSS.
+## Features
+- [x] Compare two files
+- [x] Show the differences between the files with git diff style
+
+## Future work
+- [ ] Support file upload
+- [ ] Support file drag and drop
+
+## Requirements
+- Node.js
+- yarn
+
+## Installation
 
 ```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
+yarn 
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Development
 
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
+```bash
+yarn dev
+```
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/pages/components/fileLayout.tsx
@@ -0,0 +1,133 @@
+import { Button, Grid, Modal, Textarea} from '@nextui-org/react';
+import React from "react";
+import ReactDiffViewer from 'react-diff-viewer';
+
+let oldCode = ``;
+let newCode = ``;
+
+// adjust the row number according to the screen size
+let rowNumber = 20;
+if (typeof window !== 'undefined') {
+    rowNumber = window.innerHeight / 40;
+    console.log(rowNumber, window.innerHeight);
+}
+
+export default () => {
+    const [visible, setVisible] = React.useState(false);
+    const handler = () => setVisible(true);
+    const closeHandler = () => {
+        setVisible(false);
+        console.log("closed");
+    };
+    return (
+        <div>
+            {/* Text area */}
+            {/* <div className="flex flex-row ">
+                <div className="w-full border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+                    <div className="px-4 bg-white rounded-t-lg dark:bg-gray-700">
+                        <label className="sr-only">Text Area 1</label>
+                        <textarea id="Text Area 1" className="w-full px-0 h-96 text-sm text-gray-900 bg-white border-0 dark:bg-gray-700 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="File 1 content" ></textarea>
+                    </div>
+                </div>
+                <div className="px-5" />
+                <div className="w-full border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+                    <div className="px-4 bg-white rounded-t-lg dark:bg-gray-700">
+                        <label className="sr-only">Text Area 2</label>
+                        <textarea id="Text Area 2" className="w-full px-0 h-96 text-sm text-gray-900 bg-white border-0 dark:bg-gray-700 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="File 2 content"></textarea>
+                    </div>
+                </div>
+            </div> */}
+
+            <Grid.Container gap={2}>
+                <Grid xs={12} md={6} >
+                    <Textarea
+                        placeholder="File 1 content"
+                        fullWidth={true}
+                        bordered
+                        size="xl"
+                        rows={rowNumber}
+                        onChange={(e) => {
+                            oldCode = e.target.value;
+                        }}
+                    />
+                </Grid>
+                <Grid xs={12} md={6}>
+                    <Textarea
+                        placeholder="File 2 content"
+                        fullWidth={true}
+                        bordered
+                        size="xl"
+                        rows={rowNumber}
+                        onChange={(e) => {
+                            newCode = e.target.value;
+                        }}
+                    />
+                </Grid>
+            </Grid.Container>
+
+            {/* <div className="text-2xl text-center py-8 ">
+                or upload the files
+            </div> */}
+
+            {/* File upload
+            <div className="flex flex-row ">
+                <div className="flex-1 items-center justify-center w-full">
+                    <label className="flex flex-col items-center justify-center w-full h-48 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                        <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                            <svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                            <p className="text-xs text-gray-500 dark:text-gray-400">TXT file</p>
+                        </div>
+                        <input id="file1" type="file" className="hidden" accept=".txt" />
+                    </label>
+                </div>
+                <div className="px-5" />
+                <div className="flex-1 items-center justify-center w-full">
+                    <label className="flex flex-col items-center justify-center w-full h-48 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                        <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                            <svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                            <p className="text-xs text-gray-500 dark:text-gray-400">TXT file</p>
+                        </div>
+                        <input id="file2" type="file" className="hidden" accept=".txt" />
+                    </label>
+                </div>
+            </div> */}
+            <div className='flex flex-col items-center justify-center py-10' >
+                <Button
+                    className="bg-green-800 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                    onPress={
+                        handler
+                        
+                    }>
+                    Compare
+                </Button>
+                <Modal
+                    open={visible}
+                    onClose={closeHandler}
+                    blur
+                    width='80%'
+                    // noPadding
+                    closeButton
+                    preventClose
+                    className='bg-gray-50 dark:bg-gray-800'
+                >
+                    <Modal.Body
+                        autoMargin
+                        color='gray-700'
+                    >
+                        <ReactDiffViewer
+                            oldValue={oldCode}
+                            newValue={newCode}
+                            splitView={true}
+                            showDiffOnly={false}
+                            // useDarkTheme={true}
+                        />
+                   </Modal.Body>
+                </Modal>
+
+            </div>
+        </div>
+
+    )
+}
