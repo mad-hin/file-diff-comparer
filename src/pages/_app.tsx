eee5efa9aaf22068db50565c0bd4f56588890642
Marco Tam <marcotamcn@gmail.com>
2023-07-22 20:35:34 +0800

    Change from nextui to mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,42 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { NextUIProvider } from '@nextui-org/react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
   return (
-    <NextThemesProvider defaultTheme={"dark"}>
-      <NextUIProvider >
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'dark',
+          colors: {
+            dark: [
+              // tailwindcss background gray-100 tp gray-900
+              '#F3F4F6',
+              '#E5E7EB',
+              '#D1D5DB',
+              '#9CA3AF',
+              '#6B7280',
+              '#4B5563',
+              '#374151',
+              '#1F2937',
+              '#111827',
+            ]
+          },
+        }}
+      >
         <Component {...pageProps} />
-      </NextUIProvider>
-    </NextThemesProvider>
+      </MantineProvider>
+    </>
   );
 }
